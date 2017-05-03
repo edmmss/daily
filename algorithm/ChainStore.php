22d47c60dae8cef9aec76d6d47c81f404a0f05b5
phper-cb <1227217939@qq.com>
2017-05-03 15:04:15 +0800

    Create ChainStore.php

---
+++ algorithm/ChainStore.php
@@ -0,0 +1,65 @@
+<?php 
+class Node_linked
+{
+    public $data;
+    public $next = NULL;
+    public function __construct($value = NULL)
+    {
+        $this->data = $value;
+    }
+
+    public function update($newValue)
+    {
+        $this->data = $newValue;
+    } 
+}
+
+class Linear_linked
+{
+    protected $_head;
+    public function __construct()
+    {
+        $this->_head = new Node_linked();
+    }
+
+    public function head()
+    {
+    	return $this->_head;
+    }
+
+    public function length()
+    {
+        $length = 0;
+        $prev = $this->head();
+
+        while ($prev->next)
+        {
+            $prev = $prev->next;
+            $length++;
+        }
+
+        return $length;
+    }
+
+    public function insert($value, $index)
+    {
+        $length = $this->length();
+        if ($index < 0 || $index > $length)
+        {
+            return false;
+        }
+
+        $node = new Node_linked($value);
+        $prev = $this->head();
+
+        for ($i = 0; $i < $index; $i++)
+        {
+            $prev = $prev->next;
+        }
+
+        $node->next = $prev->next;
+        $prev->next = $node;
+
+        return true;
+    }
+}
