11c736737aedcd29ea0964d9ecd4405c6c059801
陈彬 <1227217939@qq.com>
2017-08-22 14:12:38 +0800

    Update 数组打乱算法.php

--- algorithm/数组打乱算法.php
+++ algorithm/数组打乱算法.php
@@ -1,14 +1,29 @@
 <?php
 
-$arr = range(1, 20, 1);
-
-$tmpAry = [];
-$count = count($arr);
-for ($i = 0; $i < $count; $i++)
+/**
+ * className
+ *
+ * @author chenbin
+ * @param  $array      // 需要处理的数组
+ * @return array
+ */
+function arrayUpset($array)
 {
-    // 这里减一是因为数组从键从0开始
-    $num = mt_rand(0, $count - $i - 1);
-    $tmpAry[] = $arr[$num];
-    unset($arr[$num]);
-    $arr = array_values($arr);
+    $tmpAry = [];
+    $count = count($array);
+
+    for ($i = 0; $i < $count; $i++)
+    {
+        // 这里减一是因为数组从键从0开始
+        $num = mt_rand(0, $count - $i - 1);
+        $tmpAry[] = $array[$num];
+        unset($array[$num]);
+        $array = array_values($array);
+    }
+
+    return $tmpAry;
 }
+
+$arr = range(1, 20, 1);
+$data = arrayUpset($arr);
+var_dump($data);
